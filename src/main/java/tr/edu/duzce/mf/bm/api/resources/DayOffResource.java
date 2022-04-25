86767672a01b637a7154ba61f358774495d2c74d
Ebubekir YAZICI <ebubekiryazi@gmail.com>
2022-04-25 16:48:52 +0300

    backup 25.04.2022

--- src/main/java/tr/edu/duzce/mf/bm/api/resources/DayOffResource.java
+++ src/main/java/tr/edu/duzce/mf/bm/api/resources/DayOffResource.java
@@ -1,7 +1,52 @@
 package tr.edu.duzce.mf.bm.api.resources;
 
-import jakarta.annotation.Resource;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import tr.edu.duzce.mf.bm.business.abstracts.DayOffService;
+import tr.edu.duzce.mf.bm.business.concretes.DayOffManager;
+import tr.edu.duzce.mf.bm.core.utilities.results.DataResult;
+import tr.edu.duzce.mf.bm.core.utilities.results.Result;
+import tr.edu.duzce.mf.bm.dataAccess.concretes.JDBCDao.JDBCDayOffDao;
+import tr.edu.duzce.mf.bm.entities.concretes.DayOff;
 
-@Resource
+import java.util.List;
+
+@Path("/dayoffs")
 public class DayOffResource {
+    private DayOffService dayoffService;
+
+    public DayOffResource() {
+        dayoffService = new DayOffManager(new JDBCDayOffDao());
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public DataResult<List<DayOff>> getAll() {
+        return this.dayoffService.getAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DataResult<DayOff> getById(@PathParam("id") int id) {
+        return this.dayoffService.getById(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Result add(DayOff dayoff) {
+        return this.dayoffService.add(dayoff);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Result update(DayOff dayoff) {
+        return this.dayoffService.update(dayoff);
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Result delete(DayOff dayoff) {
+        return this.dayoffService.delete(dayoff);
+    }
 }
